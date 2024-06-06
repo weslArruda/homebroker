617074aba9ec69a69e6512788c440dccd93034ae
weslArruda <weslleyarrudalemospinheiro@outlook.com>
2024-06-06 14:24:29 -0300

    RequestController correction

--- p2/p2/src/main/java/com/unifacisa/p2/controller/AcaoController.java
+++ p2/p2/src/main/java/com/unifacisa/p2/controller/AcaoController.java
@@ -7,10 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.unifacisa.p2.entity.Acao;
 import com.unifacisa.p2.service.AcaoService;
 
+
+@RestController
+@RequestMapping("/acoes")
 public class AcaoController {
     @Autowired
     private AcaoService acaoService;
